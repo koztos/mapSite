5985883d1f82bddedd4f76477daff27302dd05f2
koztos <koztos4000@gmail.com>
2018-12-28 15:34:37 -0500

    fixed nothing adding lots.

--- routes/services.js
+++ routes/services.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const terminals = require('../middleware/terminals');
+const config = require('config');
 
+const key = config.get('key');
 
 router.get('/', async (req,res) => {
-
     res.render('pages/services', {
         terminals:terminals
     });
