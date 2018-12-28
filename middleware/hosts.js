1267f6a9672d167c2c6162ff5d0fa1c526b7bbe7
koztos <koztos4000@gmail.com>
2018-12-28 11:33:24 -0500

    added test ping to test page auto refresh

--- middleware/hosts.js
+++ middleware/hosts.js
@@ -1,7 +1,7 @@
 const ping = require('ping');
 
 let hosts = new Map([
-    ['Brampton', { address: '10.0.1.53', alive: false }],
+    ['Brampton', { address: '10.0.3.252', alive: false }],
     ['Vancouver', { address: '192.168.12.1', alive: false }],
     ['Calgary', { address: '192.168.5.1', alive: false }],
     ['Goreway', { address: '192.168.13.1', alive: false }],
@@ -10,15 +10,17 @@ let hosts = new Map([
     ['Halifax', { address: '192.168.7.254', alive: false }],
     ['St. Johns, NB', { address: '192.168.3.1', alive: false }],
     ['St. Johns, NL', { address: '192.168.8.1', alive: false }],
-    ['Moncton', { address: '192.168.4.254', alive: false }]
+    ['Moncton', { address: '192.168.4.254', alive: false }],
+    ['Test Laptop', { address: '10.0.2.16', alive: false }]
 ]);
 
 async function setIsAlive() {
     for (var [site,host] of hosts.entries()) {
         const isAlive = await ping.promise.probe(host.address);
         host.alive = isAlive.alive;
-        console.log(site + '-' + host.address + '-' + host.alive);
+        
     }
+    console.log(hosts);
 };
 setIsAlive();
 module.exports = hosts;
